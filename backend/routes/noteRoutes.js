d2b57a1ec6195d52031d8b2a0caeabf9ab5ee65e
Kundoty <banyuanjundao@gmail.com>
2025-06-14 00:25:09 -0400

    optimize structure, using Router

---
+++ backend/routes/noteRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("You just fetched the note")
+});
+
+router.post("/", (req, res) => {
+    res.status(201).json({message: "Note created"})
+});
+
+router.put("/:id", (req, res) => {
+    res.status(200).json({message: "Note updated"})
+});
+
+router.delete("/:id", (req, res) => {
+    res.status(200).json({message: "Note deleted"})
+});
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,9 @@
 import express from "express";
+import noteRoutes from "./routes/noteRoutes.js";
 
 const app = express();
 
-app.get("/note", (req, res) => {
-    res.send("This is a GET request response");
-});
-
-app.post("/note", (req, res) => {
-    res.status(201).json({message: "note created"})
-    
-});
-
-app.put("/note/:id", (req, res) => {
-    res.status(200).json({message: "note updated"})
-});
-
-app.delete("/note/:id", (req, res) => {
-    res.status(200).json({message: "note deleted"})
-});
+app.use("/note", noteRoutes);
 
 
 app.listen(8080, () => {});
