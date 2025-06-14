dee42178c284cc7259355ce49ba56029094e8035
Kundoty <banyuanjundao@gmail.com>
2025-06-14 00:05:28 -0400

    creat basic endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
 import express from "express";
+
+const app = express();
+
+app.get("/note", (req, res) => {
+    res.send("This is a GET request response");
+});
+
+app.post("/note", (req, res) => {
+    res.status(201).json({message: "note created"})
+    
+});
+
+app.put("/note/:id", (req, res) => {
+    res.status(200).json({message: "note updated"})
+});
+
+app.delete("/note/:id", (req, res) => {
+    res.status(200).json({message: "note deleted"})
+});
+
+
+app.listen(8080, () => {});
