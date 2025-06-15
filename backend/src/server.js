b26be3ff85d7034214bfe591dbfc3aac3c2b7e6c
Kundoty <banyuanjundao@gmail.com>
2025-06-14 23:37:45 -0400

    connect mongodb, hide uri, access it using dotenv

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use("/note", noteRoutes);
 
 
 app.listen(8080, () => {});
+
+
+// mongodb+srv://banyuanjundao:QLMsrqKDszaEA3nK@cluster0.7kp6osn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
