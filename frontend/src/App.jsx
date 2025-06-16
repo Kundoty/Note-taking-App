8a51742015be8afbeb5f49b058b9f705cad7364e
Kundoty <banyuanjundao@gmail.com>
2025-06-15 23:36:40 -0400

    add react toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { Toaster, toast } from 'react-hot-toast'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Toaster />
         <Routes>
           <Route path='/' element={<HomePage />}></Route>
           <Route path='/create' element={<CreatePage />} />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {toast} from 'react-hot-toast'
 
 const HomePage = () => {
     return (
         <div>
+            <button onClick={() => toast.success("成功")}>click</button>
             This is home page
         </div>
     )
