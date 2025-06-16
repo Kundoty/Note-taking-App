ae49369c44c8df5aac42e56aeb869511a18ed153
Kundoty <banyuanjundao@gmail.com>
2025-06-15 23:23:21 -0400

    create some pages and route them

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePage = () => {
+    return (
+        <div>
+            This is the page to create note
+        </div>
+    )
+}
+
+export default CreatePage
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteDetailPage = () => {
+    return (
+        <div>
+            This is page of note details
+        </div>
+    )
+}
+
+export default NoteDetailPage
