c6f81f4bed04346dedbb766c6e788e6814d1920a
Kundoty <banyuanjundao@gmail.com>
2025-06-18 23:23:26 -0400

    display note card component on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 import Navbar from '../components/Navbar.jsx'
 import RatelimitUI from '../components/RatelimitUI.jsx'
+import Notecard from '../components/Notecard.jsx'
 
 const HomePage = () => {
     const [isRatelimited, setIsRatelimited] = useState(false);
@@ -18,7 +19,7 @@ const HomePage = () => {
                 const res = await axios.get('http://localhost:8080/note');
                 setNotes(res.data);
                 setIsRatelimited(false);
-                console.log(res.data);
+                //console.log(res.data);
             } catch (error) {
                 console.error("Error fetching notes:", error);
                 if (error.response.status === 429) {
@@ -42,8 +43,16 @@ const HomePage = () => {
             <Navbar />
             { isRatelimited && <RatelimitUI /> }
             <div className='max-w-7xl mx-auto p-4 mt-6'>
-                {loading && <div className='text-center text-primary py-10'>Loading ...</div>}
-                {notes.length > 0 && !isRatelimited && (1)}
+                {loading && <div className='text-center text-primary py-10'>正在加载你的笔记 ...</div>}
+                {notes.length > 0 && !isRatelimited && (
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                        {notes.map((note, id) => (
+                            <div>
+                                <Notecard note={note}/>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     )
