16e456d44384a9c9880f17d1371428ac63d53ff5
Kundoty <banyuanjundao@gmail.com>
2025-06-20 20:35:34 -0400

    create UI when the board has no notes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <header className='bg-base-300 border-b border-base-content'>
             <div className='mx-auto max-w-6xl p-4'>
                 <div className='flex items-center justify-between'>
-                    <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>NoteBoard</h1>
+                    <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>猫咪记事本</h1>
                     <div className='flex items-center gap-4'>
                         <Link to={"/create"} className='btn btn-primary'>
                             <PlusIcon className='size-5'/>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import Navbar from '../components/Navbar.jsx'
 import RatelimitUI from '../components/RatelimitUI.jsx'
 import Notecard from '../components/Notecard.jsx'
+import EmptyBoard from '../components/EmptyBoard.jsx'
 
 const HomePage = () => {
     const [isRatelimited, setIsRatelimited] = useState(false);
@@ -44,6 +45,7 @@ const HomePage = () => {
             { isRatelimited && <RatelimitUI /> }
             <div className='max-w-7xl mx-auto p-4 mt-6'>
                 {loading && <div className='text-center text-primary py-10'>正在加载你的笔记 ...</div>}
+                {noteList.length === 0 && !loading && !isRatelimited && <EmptyBoard />}
                 {noteList.length > 0 && !isRatelimited && (
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                         {noteList.map((note, id) => (
